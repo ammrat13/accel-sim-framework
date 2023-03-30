da078bcc2eea0b54f7979e9ad55151094b1e059b
Ammar Ratnani <ammrat13@gmail.com>
2023-03-30 16:50:19 -0400

    Change registers used

--- util/gasan/gasan_pkg/context.py
+++ util/gasan/gasan_pkg/context.py
@@ -1,27 +1,32 @@
 from typing import Optional
+from sys import stderr
 
 class Context:
     """Represents global information about the file currently being processed.
     Most importantly, it contains the registers to use when generating GASAN
     instructions.
 
-    :ivar Optional[str] r_addr: The extra register to use for address
-        calculation
-    :ivar Optional[str] r_off: The extra register to use to store and compare
-        the offset
+    :ivar Optional[str] r_block_val: Register to compute the address and hold
+        the value of the "block" of shadow memory
+    :ivar Optional[str] r_block_off: Register to hold the offset of the address
+        into the "block" represented by shadow memory
+    :ivar Optional[str] r_shadow_base: Register to store the base address of
+        shadow memory
     """
 
     def __init__(self):
         # Set the variables to default values
-        self.r_addr = None
-        self.r_off = None
+        self.r_block_val = None
+        self.r_block_off = None
+        self.r_shadow_base = None
 
     def is_finalized(self) -> bool:
         """Whether all of the context information has a non-None value. This
         should always return true during instruction processing.
         """
-        return self.r_addr != None and \
-            self.r_off != None
+        return self.r_block_val is not None and \
+            self.r_block_off is not None and \
+            self.r_shadow_base is not None
 
     def process_line(self, line: str) -> Optional[str]:
         """Update the context information based on a non-instruction line. May
@@ -33,17 +38,20 @@ class Context:
 
         # Use register number information
         if line.startswith("-nregs = "):
-            nregs = int(line.split("=")[1])
+            old_nregs = int(line.split("=")[1])
+
+            # Compute the new number of registers
+            # Warn if there's not enough space
+            if old_nregs > 255 - 3:
+                print(f"WARN: Not enough registers with -nregs = {old_nregs}", file=stderr)
+            new_nregs = min(255, old_nregs + 3)
 
             # Compute the register numbers to use
-            r_addr_num = min(253, nregs + 0)
-            r_off_num = min(254, nregs + 1)
-            # Write into variables
-            self.r_addr = f"R{r_addr_num}"
-            self.r_off = f"R{r_off_num}"
+            self.r_block_val = f"R{new_nregs - 3}"
+            self.r_block_off = f"R{new_nregs - 2}"
+            self.r_shadow_base = f"R{new_nregs - 1}"
 
             # Write the new nregs
-            new_nregs = min(255, nregs + 2)
             return f"-nregs = {new_nregs}\n"
 
         # Do nothing by default
