4469fdb8465c6e5804bcd3cf2bc25b331f38d77b
Ammar Ratnani <ammrat13@gmail.com>
2023-03-31 01:03:25 -0400

    Add variants

--- util/gasan/gasan_pkg/context.py
+++ util/gasan/gasan_pkg/context.py
@@ -13,6 +13,7 @@ class Context:
     :ivar Optional[str] r_shadow_base: Register to store the base address of
         shadow memory
     :ivar int SHADOW_BASE: Base address of shadow memory
+    :ivar int VARIANT: Either 128 or 256
     """
 
     def __init__(self):
@@ -21,6 +22,7 @@ class Context:
         self.r_block_off = None
         self.r_shadow_base = None
         self.SHADOW_BASE = 0x700000000000
+        self.VARIANT = 128
 
     def is_finalized(self) -> bool:
         """Whether all of the context information has a non-None value. This
